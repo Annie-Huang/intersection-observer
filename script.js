a541586936ef156289da6d6c9dddc94852031db2
Annie Huang <annie.hn@gmail.com>
2023-08-11 16:33:45 +1000

    Accidentally turn off auto reformat on save. Turn it back on

--- script.js
+++ script.js
@@ -26,36 +26,38 @@ Each intersectionObserverEntry has the following attributes {
   time
 }
 */
-const observer = new IntersectionObserver((entries) => {
-  console.log(entries);
-  entries.forEach(entry => {
-    entry.target.classList.toggle('show', entry.isIntersecting)
+const observer = new IntersectionObserver(
+  (entries) => {
+    console.log(entries);
+    entries.forEach((entry) => {
+      entry.target.classList.toggle('show', entry.isIntersecting);
 
-    // As soon as the target is in the page and we add .show, we stop observing, meaning leave it to the page even if we scroll up again.
-    // if(entry.isIntersecting) observer.unobserve(entry.target);
-  })
-}, {
-  // threshold default is 0, meaning as soon as the first pixel is visible. You can change it 1, meaning only the whole target is visible, then we do the logic.
-  // this also means as soon as the first pixel disappear in the top card (when scrolling down), we will make the card disappear.
-  threshold: 1,
+      // As soon as the target is in the page and we add .show, we stop observing, meaning leave it to the page even if we scroll up again.
+      // if(entry.isIntersecting) observer.unobserve(entry.target);
+    });
+  },
+  {
+    // threshold default is 0, meaning as soon as the first pixel is visible. You can change it 1, meaning only the whole target is visible, then we do the logic.
+    // this also means as soon as the first pixel disappear in the top card (when scrolling down), we will make the card disappear.
+    threshold: 1,
 
-  // rootMargin meaning subtract (for negative value) the container (e.g. the page) top and bottom by 100px each.
-  // When you enable this, disable 'threshold: 1', otherwise it will go into infinite loop.
-  // We usually have this in position value. E.g. rootMargin: "100px", can mean when we are about 100px from the actual image, start down loading the image.
-  // rootMargin: "-100px",
+    // rootMargin meaning subtract (for negative value) the container (e.g. the page) top and bottom by 100px each.
+    // When you enable this, disable 'threshold: 1', otherwise it will go into infinite loop.
+    // We usually have this in position value. E.g. rootMargin: "100px", can mean when we are about 100px from the actual image, start down loading the image.
+    // rootMargin: "-100px",
 
-  // you can define a root container, as long as it's scrollable, it will work. But 99% of time you will not need to change the this property, because you usually left it to observe the entire page.
-  // root:
-});
+    // you can define a root container, as long as it's scrollable, it will work. But 99% of time you will not need to change the this property, because you usually left it to observe the entire page.
+    // root:
+  }
+);
 
-const lastCardObserver = new IntersectionObserver(entries => {
-  const lastCard = entries[0]
-}, {})
+const lastCardObserver = new IntersectionObserver((entries) => {
+  const lastCard = entries[0];
+}, {});
 
-lastCardObserver.observe(document.querySelector('.card:last-child'))
-
+lastCardObserver.observe(document.querySelector('.card:last-child'));
 
 // observer.observe(cards[0]);
 // You only see if it you move faster, because by default as soon as a single pixel is show on the screen, we will add .show class into the card.
 // That is why we need the threshold option.
-cards.forEach(card => observer.observe(card))
+cards.forEach((card) => observer.observe(card));
